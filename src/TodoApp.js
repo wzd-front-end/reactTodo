4ce3b302c0cbbc66f83734df1154f491e1d29390
wuzd@mybilin.cn <wuzd@mybilin.cn>
2019-02-15 14:38:57 +0800

    高级用法，函数子组件实现倒计时

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import {view as Todos} from './todos';
 import {view as Filter} from './filter';
+import {view as CountDown} from './countDown'
+
+function showCount(count) {
+    return (<div>{count > 0 ? count : '新年快乐'}</div>)
+}
 
 function TodoApp() {
     return (
         <div>
             <Todos/>
             <Filter/>
+            <CountDown startCount={10}>
+                {
+                    showCount
+                }
+            </CountDown>
         </div>
     )
 }
--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -24,11 +24,10 @@ const TodoList = ({todos, onToggleTodo, onRemoveTodo}) => {
         </ul>
     )
 }
+
 TodoList.propTypes = {
     todos: PropTypes.array.isRequired
 }
-
-
 
 const mapStateToProps = (state) => {
     return {
@@ -47,6 +46,7 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
+
 // 单个组件优化方法一：利用react-redux的connect方法，封装返回的组件类其实是包含shouldComponentUpdate生命周期的实现了，
 // 只要prop不发生改变，是不会返回true，因为，我们只需要控制props属性值不发生一些没必要的更新即可减少组件的重新渲染，
 // 这个就包括了传入值是对象的值的初始化，以及传入带参数的函数的初始化，因为带参数的函数实际是一个匿名函数，每次初始化都会产生一个新的函数，
