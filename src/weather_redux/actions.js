e0352e50d5bc91c593c7e64a5d6ddf15232e1a43
wuzd@mybilin.cn <wuzd@mybilin.cn>
2019-02-18 14:39:20 +0800

    新增由redux 控制异步数据获取的组件

---
+++ src/weather_redux/actions.js
@@ -0,0 +1,37 @@
+import {FETCH_FAILURE, FETCH_STARTED, FETCH_SUCCESS} from './actionTypes'
+
+export const fetchWeatherStarted = () => ({
+    type:FETCH_STARTED
+});
+
+export const fetchWeatherSuccess = (result) => ({
+    type:FETCH_SUCCESS,
+    result
+});
+
+export const fetchWeatherFailure = (error) => ({
+    type:FETCH_FAILURE,
+    error
+});
+
+export const fetchWeather = (cityCode)=>{
+    return (dispatch) => {
+        const apiUrl = `/data/cityinfo/${cityCode}.html`;
+
+        dispatch(fetchWeatherStarted());
+
+        return fetch(apiUrl).then((respone) => {
+            if(respone.status !== 200){
+                throw new Error('Fail to get response with status ' + response.status);
+            }
+
+            respone.json().then((responeJson) => {
+                dispatch(fetchWeatherSuccess(responeJson.weatherinfo));
+            }).catch((error) => {
+                dispatch(fetchWeatherFailure(error));
+            })
+        }).catch((error) => {
+            dispatch(fetchWeatherFailure(error))
+        })
+    }
+};
---
+++ src/weather_redux/view.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {connet} from 'react-redux';
+import propTypes from 'prop-types'
+import * as Status from './status';
+
+const weather = ({status, cityName, weather, lowestTemp, highestTemp}) => {
+    switch (status) {
+        case Status.LOADING: {
+            return <div>天气信息请求中...</div>
+        }
+
+        case Status.SUCCESS: {
+            return (
+                <div>
+                    {cityName} {weather} 最低气温 {lowestTemp} 最高气温 {highestTemp}
+                </div>
+            )
+        }
+
+        case Status.FAILURE: {
+            return <div>天气信息装载失败</div>
+        }
+
+        default: {
+            throw new Error('unexpected status ' + status);
+        }
+    }
+}
+
+weather.propTypes = {
+    status: propTypes.string.isRequired,
+    cityName: propTypes.string,
+    weather: propTypes.string,
+    lowestTemp: propTypes.string,
+    highestTemp: propTypes.string
+};
+
+const mapStateToProps = (state) => {
+    const weatherData = state.weather;
+
+    return {
+        status: weatherData.status,
+        cityName: weatherData.city,
+        weather: weatherData.weather,
+        lowestTemp: weatherData.temp1,
+        highestTemp: weatherData.temp2
+    }
+};
+
+export default connet(mapStateToProps)(Weather);
